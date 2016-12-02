a3fa579e46318c9dd7a80c623a19df50724253bf
Nandan Rao <nandanmarkrao@gmail.com>
2016-12-02 13:28:49 +0100

    bumped

--- modelling/clustering.py
+++ modelling/clustering.py
@@ -1,32 +1,29 @@
-from modelling.text_processing import split_into_lemmas, split_into_tokens, remove_stop_words
-
-##GENERAL PACKAGES
-import time
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import cPickle
-
-import sklearn
-from sklearn import tree
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC, LinearSVC
-from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
-from sklearn.pipeline import Pipeline
-from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import StratifiedKFold, cross_val_score, train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.learning_curve import learning_curve
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from pymongo import MongoClient
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import DBSCAN
+from models import clean_html
 
+def get_articles_by_source(client, src = '', label = ''):
+    collection = client['newsfilter'].news #news - is new one
+    cursor = collection.find({ '_id': {'$regex': src }, 'label': {'$regex': label}})
+    return list(cursor)
 
-def create_model(msg_train):
+def get_all_articles(client):
+    real = get_articles_by_source(client, 'ge')
+    fake = get_articles_by_source(client, 'fa')
+    return real + fake
 
+def get_all_tweets(client):
+    tweets = get_articles_by_source(client, 'tw')
+    return tweets
 
+def dbscan(data, epsilon):
+    vector = CountVectorizer(
+        stop_words='english',
+        ngram_range=(2,3),
+        preprocessor = clean_html
+    ).fit_transform(data)
 
-    dbscan =  DBSCAN(pipeline, params)
-    return dbscan(msg_train)
+    db = DBSCAN(eps=epsilon, min_samples=2)
+    fit = db.fit_predict(vector)
+    return fit
