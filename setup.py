059dff938685582c7020170b5d349c8d4167ca58
Nandan Rao <nandanmarkrao@gmail.com>
2016-12-02 12:33:13 +0100

    bumped

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="modelling",
-    version='0.0.3',
+    version='0.0.4',
     url="https://github.com/migrantnewsfilter/Modelling",
 
     py_modules=['modelling'],
@@ -11,7 +11,7 @@ setup(
 
     install_requires=[
         'pandas',
-        'MongoClient',
+        'pymongo',
         'numpy',
         'sklearn',
         'scipy'
