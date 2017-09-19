8c897f0d935cf4f9196135b16911c8dbefb44516
Nandan Rao <nandanmarkrao@gmail.com>
2017-09-19 11:06:03 +0200

    get_unique_articles using get_articles

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -3,12 +3,11 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import DBSCAN
+from modelling.utils import get_articles
 
 def get_labelled_articles(uri = None):
-    client = MongoClient(uri)
-    collection = client['newsfilter'].news
-    cursor = collection.find({ 'label': {'$exists': True}})
-    return list(cursor)
+    collection = MongoClient(uri)['newsfilter'].news
+    return get_articles(collection, label = True)
 
 def create_df(li):
     original = pd.DataFrame(li)
