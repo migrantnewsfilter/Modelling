3e88c45ec98d5d8b3f74c5c91340dd80d77450ae
Nandan Rao <nandanmarkrao@gmail.com>
2017-09-26 08:40:01 +0200

    opening up dbscan algorithms

--- modelling/clustering.py
+++ modelling/clustering.py
@@ -3,17 +3,18 @@ from pymongo import MongoClient
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.cluster import DBSCAN
 from modelling.models import clean_html
+from modelling.utils import get_articles
 import pandas as pd
 import numpy as np
 
-def dbscan(data, epsilon, n = 2):
+def dbscan(data, epsilon, n = 2, leaf_size = 30):
     vector = CountVectorizer(
         stop_words='english',
         ngram_range=(1,2),
         preprocessor = clean_html
     ).fit_transform(data)
 
-    db = DBSCAN(eps=epsilon, min_samples=n, algorithm='brute', metric='cosine')
+    db = DBSCAN(eps=epsilon, min_samples=n, leaf_size=leaf_size, algorithm=algorithm)
     fit = db.fit_predict(vector)
     return fit
 
@@ -29,7 +30,7 @@ def get_unique_items(df, epsilon, body = 'body'):
 def get_cluster_table(epsilon):
     """ Used for optimizing and testing interactively """
     collection = MongoClient()['newsfilter'].news
-    tweets = get_all_articles(collection)
+    tweets = get_articles(collection, src='tw')
     bodies = map(lambda x: x['content'].get('body'), tweets)
     fit = dbscan(bodies, epsilon)
     zipped = zip(fit, bodies)
