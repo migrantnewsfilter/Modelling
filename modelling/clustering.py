f48677ecd0703f849a59da9aa5214bb8431b43aa
Nandan Rao <nandanmarkrao@gmail.com>
2017-09-30 15:57:06 +0200

    Clustering takes DBSCAN as arg.

--- modelling/clustering.py
+++ modelling/clustering.py
@@ -6,33 +6,35 @@ from modelling.models import clean_html
 from modelling.utils import get_articles
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
-def dbscan(data, epsilon, n = 2, leaf_size = 30, algorithm = 'auto'):
+def dbscan(data, db):
     vector = CountVectorizer(
         stop_words='english',
         ngram_range=(1,2),
         preprocessor = clean_html
     ).fit_transform(data)
 
-    db = DBSCAN(eps=epsilon, min_samples=n, leaf_size=leaf_size, algorithm=algorithm)
-    fit = db.fit_predict(vector)
-    return fit
-
+    return db.fit_predict(vector)
 
 def get_unique_items(df, epsilon, body = 'body'):
-    clusters = dbscan(df[body], epsilon)
+    db = DBSCAN(eps = epsilon, min_samples = 2,
+                algorithm = 'brute', metric = 'cosine')
+
+    clusters = dbscan(df[body], db)
     df = df.assign(cluster = clusters)
     df_clusters = (df[df.cluster != -1].groupby('cluster')
                    .first())
     return pd.concat([df_clusters, df[df.cluster == -1]])
 
 
-def get_cluster_table(epsilon):
+def get_cluster_table(eps, body = 'body', src = 'tw', date = datetime(2017,9,26), db = None):
     """ Used for optimizing and testing interactively """
+    db = DBSCAN(eps = eps, min_samples = 2, algorithm = 'brute', metric = 'cosine') if not db else db
     collection = MongoClient()['newsfilter'].news
-    tweets = get_articles(collection, src='tw')
-    bodies = map(lambda x: x['content'].get('body'), tweets)
-    fit = dbscan(bodies, epsilon)
+    tweets = get_articles(collection, src=src, date_start = date)
+    bodies = list(map(lambda x: x['content'].get(body), tweets))
+    fit = dbscan(bodies, db)
     zipped = zip(fit, bodies)
     z = sorted(zipped, key = lambda a: a[0])
     z.reverse()
