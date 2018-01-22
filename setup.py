feb9b82967a52f98c05d593be81e1a376d7338d4
Nandan Rao <nandanmarkrao@gmail.com>
2018-01-22 10:09:55 +0100

    Bump ^ 0.19

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="modelling",
-    version='0.0.18',
+    version='0.0.19',
     url="https://github.com/migrantnewsfilter/Modelling",
 
     py_modules=['modelling'],
