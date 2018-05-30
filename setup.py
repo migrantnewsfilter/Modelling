a3060f8c3145005683e1d711e2edfe9d32f3b28f
Nandan Rao <nandanmarkrao@gmail.com>
2018-05-30 09:58:26 +0200

    ^BUMP v.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="modelling",
-    version='0.0.19',
+    version='0.0.20',
     url="https://github.com/migrantnewsfilter/Modelling",
 
     py_modules=['modelling'],
